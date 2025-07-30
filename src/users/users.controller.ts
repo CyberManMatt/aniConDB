37a3a66983873fe3f68cedd05936104b0e998094
Matt Jarrett <mtthwjarrett0@gmail.com>
2025-07-30 20:18:03 +0000

    Add authentication decorator and enum for authorization types

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
 import { UsersService } from './providers/users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { AuthType } from 'src/auth/enums/auth-type.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Auth(AuthType.None)
   @ApiResponse({ status: 201, description: 'Profile created successfully.' })
   @ApiResponse({ status: 400, description: 'Bad Request.' })
   @ApiOperation({ summary: 'Create a new user (Sign Up)' })
